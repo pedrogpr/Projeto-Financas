d98759afd915b82fb42bd5bac64ff7b4dc491309
Pedro Reis <pedro.gui6442@gmail.com>
2022-07-11 22:57:52 -0300

    Service e Exception

--- minhasfinancas/src/main/java/com/projeto/minhasfinancas/model/repository/UsuarioRepository.java
+++ minhasfinancas/src/main/java/com/projeto/minhasfinancas/model/repository/UsuarioRepository.java
@@ -3,5 +3,11 @@ package com.projeto.minhasfinancas.model.repository;
 import com.projeto.minhasfinancas.model.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+    boolean existsByEmail(String email);
+
+    Optional<Usuario> findByEmail(String email);
 }
--- minhasfinancas/src/main/java/com/projeto/minhasfinancas/service/impl/UsuarioServiceImpl.java
+++ minhasfinancas/src/main/java/com/projeto/minhasfinancas/service/impl/UsuarioServiceImpl.java
@@ -1,25 +1,52 @@
 package com.projeto.minhasfinancas.service.impl;
 
+import com.projeto.minhasfinancas.exception.ErroAutenticacao;
+import com.projeto.minhasfinancas.exception.RegraNegocioException;
 import com.projeto.minhasfinancas.model.entity.Usuario;
 import com.projeto.minhasfinancas.model.repository.UsuarioRepository;
 import com.projeto.minhasfinancas.service.UsuarioService;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
+
+@Service
 public class UsuarioServiceImpl implements UsuarioService {
 
     private UsuarioRepository repository;
 
-    @Override
-    public Usuario autenticar(String email, String senha) {
-        return null;
+    public UsuarioServiceImpl(UsuarioRepository repository) {
+        super();
+        this.repository = repository;
     }
 
     @Override
+    public Usuario autenticar(String email, String senha) {
+        Optional<Usuario> usuario = repository.findByEmail(email);
+
+        if(!usuario.isPresent()) {
+            throw new ErroAutenticacao("Usuário não encontrado.");
+        }
+
+        if(!usuario.get().getSenha().equals(senha)) {
+            throw new ErroAutenticacao("Senha inválida.");
+        }
+
+        return usuario.get();
+    }
+
+    @Override
+    @Transactional
     public Usuario salvarUsuario(Usuario usuario) {
-        return null;
+        validarEmail(usuario.getEmail());
+        return repository.save(usuario);
     }
 
     @Override
     public void validarEmail(String email) {
-
+        boolean existe = repository.existsByEmail(email);
+        if(existe) {
+            throw new RegraNegocioException("Já existe um usuário cadastrado com este e-mail.");
+        }
     }
 }
