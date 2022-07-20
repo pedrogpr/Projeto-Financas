826b4531c6a4366232ed203613f33e914c2556e9
Pedro Reis <pedro.gui6442@gmail.com>
2022-07-19 23:13:14 -0300

    Adicionado alguns componentes

--- minhasfinancas-app/src/App.js
+++ minhasfinancas-app/src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { useState } from 'react';
-import 'bootswatch/dist/flatly/bootstrap.css';
 import Login from './views/login';
+
+import 'bootswatch/dist/flatly/bootstrap.css';
+import './App.css'
 
 function App() {
   return (
--- minhasfinancas-app/src/views/login.js
+++ minhasfinancas-app/src/views/login.js
@@ -1,7 +1,17 @@
 import React from "react";
 import Card from "../components/card";
+import FormGroup from "../components/form-group";
+import { useState } from "react";
 
 function Login() {
+
+  const [email, setEmail] = useState('')
+  const [senha, setSenha] = useState('')
+
+  const entrar = () => {
+
+  }
+
   return (
     <div className="container">
       <div className="row">
@@ -12,7 +22,25 @@ function Login() {
                 <div className="col-lg-12">
                   <div className="bs-component">
                     <fieldset>
-
+                      <FormGroup label="Email: *" htmlFor="exampleInputEmail1">
+                        <input type="email"
+                          value={email}
+                          onChange={e => setEmail(e.target.value)}
+                          className="form-control"
+                          id="exampleInputEmail1"
+                          aria-describedby="emailHelp"
+                          placeholder="Digite o Email" />
+                      </FormGroup>
+                      <FormGroup label="Senha: *" htmlFor="exampleInputPassword1">
+                        <input type="password"
+                          value={senha}
+                          onChange={e => setSenha(e.target.value)}
+                          className="form-control"
+                          id="exampleInputPassword1"
+                          placeholder="Password" />
+                      </FormGroup>
+                      <button onClick={entrar} className="btn btn-success">Entrar</button>
+                      <button className="btn btn-danger">Cadastrar</button>
                     </fieldset>
                   </div>
                 </div>
